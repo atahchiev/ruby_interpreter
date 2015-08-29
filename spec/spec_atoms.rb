25981d8add059fa6d1c399981e52cb03d28e2e6b
Alexander Tahchiev <alexander.tahchiev@gmail.com>
2015-08-29 14:07:19 +0300

    Normalization

--- spec/spec_atoms.rb
+++ spec/spec_atoms.rb
@@ -1,10 +1,4 @@
-
-
-# require_relative "./parser.rb"
-require "test/unit"
-require "../lib/lisputin/parser"
-# require_relative "spec_arithmetic"
-# p Lisputin::Parser.parse('2').to_sexp
+require "spec_helper"
 
 class TestAtoms < Test::Unit::TestCase
   def test_integer
@@ -22,14 +16,8 @@ class TestAtoms < Test::Unit::TestCase
     assert_equal(Lisputin::Parser.parse('-3/4').to_sexp, [[:rat, -3, 4]] )
   end
 
-
   def test_complex
     assert_equal(Lisputin::Parser.parse('3 + 4i').to_sexp, [[:complex, [:integer, 3], [:integer, 4]]] )
     assert_equal(Lisputin::Parser.parse('-2+3i').to_sexp, [[:complex, [:integer, -2], [:integer, 3]]] )
   end
-    #   assert_equal(Lisputin::Parser.parse('2.4').to_sexp, [[:integer, 2]] )
-    # assert_equal(Lisputin::Parser.parse('2/10').to_sexp, [[:rat, "2", "10"]] )
-    # assert_equal(Lisputin::Parser.parse('2').to_sexp, [[:integer, 2]] )
-
-
 end
--- spec/spec_functions.rb
+++ spec/spec_functions.rb
@@ -1,20 +1,8 @@
-
-
-# require_relative "./parser.rb"
-require "test/unit"
-require "../lib/lisputin/parser"
-# require_relative "spec_arithmetic"
-# p Lisputin::Parser.parse('2').to_sexp
+require "spec_helper"
 
 class TestAtoms < Test::Unit::TestCase
   def test_function
     assert_equal(Lisputin::Parser.parse('(define cat (a b c) (+ a b c))').to_sexp, [[[:func, :cat], [:args, [:a, :b, :c]], [[:op, :+], [[:symbol, :a], [:symbol, :b], [:symbol, :c]]]]] )
     assert_equal(Lisputin::Parser.parse('(define dog () ())').to_sexp, [[[:func, :dog], [:args, []], [:args, []]]] )
   end
-
-    #   assert_equal(Lisputin::Parser.parse('2.4').to_sexp, [[:integer, 2]] )
-    # assert_equal(Lisputin::Parser.parse('2/10').to_sexp, [[:rat, "2", "10"]] )
-    # assert_equal(Lisputin::Parser.parse('2').to_sexp, [[:integer, 2]] )
-
-
 end
